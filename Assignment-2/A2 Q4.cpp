70eb26629bdb45b0db26bee02c39c7b2f74b046a
SamarthBadola <samarthbadola@gmail.com>
2025-08-21 06:44:58 +0530

    Update A2 Q4.cpp

--- Assignment-2/A2 Q4.cpp
+++ Assignment-2/A2 Q4.cpp
@@ -1,3 +1,4 @@
+// (a)Code to concatenate two strings
 #include <iostream>
 using namespace std;
 #define MAX 100
@@ -20,4 +21,29 @@ int main() {
      str1[i]='\0';
     cout << "Concatenated string:\n"<<str1;
     return 0;
+}
+// (b) Code to reverse string
+#include <iostream>
+using namespace std;
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+    int l=0, j=0;
+    while (str[l]!='\0'){
+        l++;
+    }
+      for (int i=0,j=l-1;i<j;i++,j--){
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+    j=0;
+    cout<<"Reversed string: ";
+    while (str[j] !='\0'){
+        cout<<str[j];
+        j++;
+    }
+    return 0;
+    // (c) program to delete all vowels from string
 }
