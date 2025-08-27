d9a0b54c7e03fb0b1ed1dbc606c8df051f3380b2
SamarthBadola <samarthbadola@gmail.com>
2025-08-27 17:15:48 +0530

    Update A2 Q4.cpp

--- Assignment-2/A2 Q4.cpp
+++ Assignment-2/A2 Q4.cpp
@@ -45,5 +45,68 @@ int main() {
         j++;
     }
     return 0;
-    // (c) program to delete all vowels from string
+}
+// (c) program to delete all vowels from string
+#include <iostream>
+using namespace std;
+#define max 100
+int main() {
+    char str[max], result[max];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    int i = 0, j = 0;
+    while (str[i] != '\0') {
+        char c = str[i];
+        if (!(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+              c=='A'||c=='E'||c=='I'||c=='O'||c=='U')) {
+            result[j] = c;
+            j++;
+        }
+        i++;
+    }
+    cout << "String after removing vowels: " << result << endl;
+    return 0;
+}
+// (d) program to sort the strings in alphabetical order.
+#include <iostream>
+using namespace std;
+void SortString(char str[]) {
+    int l=0;
+    while(str[l]!='\0'){
+        l++;
+    }
+    for(int i=0;i<l-1;i++){
+        for (int j=i+1;j<l;j++) {
+            if (str[i] > str[j]) {
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
+int main() {
+    char str[100];
+    cout<<"Enter a string: ";
+    cin.getline(str,100);
+    SortString(str);
+    cout<<"String in alphabetical order: "<<str<<endl;
+    return 0;
+}
+// (e) program to convert a character from uppercase to lowercase.
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter an uppercase character: ";
+    cin>>ch;
+    if(ch>='A' && ch<='Z'){
+        ch=ch+32;
+        cout<<"Lowercase character: "<<ch<<endl;
+    } 
+    else{
+        cout<<"Not an uppercase character."<<endl;
+    }
+    return 0;
 }
