a3de6a7b84f494a07b6c3d6b08595586b675df29
SamarthBadola <samarthbadola@gmail.com>
2025-09-05 14:22:24 +0530

    Update A2 Q5.cpp

--- Assignment-2/A2 Q5.cpp
+++ Assignment-2/A2 Q5.cpp
@@ -1,8 +1,9 @@
 // (a) diagonal Matrix
 #include<iostream>
+#define max 100
 using namespace std;
 int main(){
-    int m[50], n;
+    int m[max], n;
     cout<<"what is the size of the matrix?\n";
     cin>>n;
     cout<<"enter "<<n<<" elements of the diagonal matrix\n";
@@ -23,7 +24,7 @@ int main(){
     }
     //(b) tri-diagonal matrix
     int size= 3*n-2;
-    int t[100];          
+    int t[max];          
     cout<<"Enter "<<size <<" elements of the tri-diagonal matrix\n";
     cout<<"(first " <<n-1<<" sub-diagonal, then "<< n<<" main, then "<< n-1<<" super-diagonal):\n";
     for(int i=0;i< size;i++){
@@ -33,16 +34,38 @@ int main(){
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(i-j==1){
-                cout <<t[i-1]<< " ";
+                cout <<t[i-1]<<" ";
             } 
             else if(i==j){       
                 cout<<t[n-1+i]<<" ";
             }
             else if(j-i==1){      
-                cout<<t[2*n-1+i]<< " ";
+                cout<<t[2*n-1+i]<<" ";
             } 
             else{
                 cout<<"0 ";
+            }
+        }
+        cout<<endl;
+    }
+    //(c) Lower Triangular Matrix
+    int store=n*(n+1)/2;
+    int l[max];
+    cout<<"Enter "<<store<<" Values for the lower triangular matrix\n(diagonal first then remaining elements row-wise)\n";
+    for(int i=0;i<store;i++){
+    cin>>l[i];
+    }
+    cout<<"Lower triangular Matrix:\n";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                cout<<" "<<l[i];
+            }
+            else if(i>j){
+                cout<<" "<<l[n+i*(i-1)/2+j];
+            }
+            else{
+                cout<<" 0";
             }
         }
         cout<<endl;
