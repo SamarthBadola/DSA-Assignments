df71ddaa9b1fdda4341aca10e41a8d7bcf2c8b9b
SamarthBadola <samarthbadola@gmail.com>
2025-09-05 13:20:52 +0530

    Update A2 Q5.cpp

--- Assignment-2/A2 Q5.cpp
+++ Assignment-2/A2 Q5.cpp
@@ -1,5 +1,5 @@
-// (a) Diagonal Matrix
-#include <iostream>
+// (a) diagonal Matrix
+#include<iostream>
 using namespace std;
 int main(){
     int m[50], n;
@@ -20,6 +20,32 @@ int main(){
             }
         }
         cout<<" "<<endl;
+    }
+    //(b) tri-diagonal matrix
+    int size= 3*n-2;
+    int t[100];          
+    cout<<"Enter "<<size <<" elements of the tri-diagonal matrix\n";
+    cout<<"(first " <<n-1<<" sub-diagonal, then "<< n<<" main, then "<< n-1<<" super-diagonal):\n";
+    for(int i=0;i< size;i++){
+        cin >> t[i];
+    }
+    cout<<"\nThe tri-diagonal matrix is:\n";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i-j==1){
+                cout <<t[i-1]<< " ";
+            } 
+            else if(i==j){       
+                cout<<t[n-1+i]<<" ";
+            }
+            else if(j-i==1){      
+                cout<<t[2*n-1+i]<< " ";
+            } 
+            else{
+                cout<<"0 ";
+            }
+        }
+        cout<<endl;
     }
     return 0;
 }
